8ca6d9dc03d980e8c4fa2a1e4ae7bbd00e4bb7d4
bobert <67173136+Shappybappy@users.noreply.github.com>
2022-04-30 15:50:48 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+#main.py
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+
+from bs4 import BeautifulSoup
+
+DRIVER_PATH = "C:\\Users\\rmrey\\shitty apps\\ChromeDriver\\chromedriver.exe"
+main_url = 'https://www.escapefromtarkov.com/cash'
+
+options = Options()
+options.headless = True
+options.add_argument("--window-size=1920,1200")
+
+driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
+
+def find_results(url, result):
+    driver.get(url)
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+    titles = soup.find_all(class_='title')
+    count = 0
+    for title in titles:
+        result.extend(title.find(class_='text'))
+        if count < 1:
+            result.extend(soup.find(class_='count'))
+            result.extend(soup.find(class_='count_left'))
+            count += 1
+
+    return result
+
+if __name__ == '__main__':
+    print(find_results(main_url, []))
