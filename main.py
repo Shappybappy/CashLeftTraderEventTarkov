a314f88aab0d372bb1ead150f1a0c0eb44c37a8f
bobert <67173136+Shappybappy@users.noreply.github.com>
2022-05-01 07:35:47 -0500

    changed a lot of things
    replaced for loop with list comprehension
    made it so it doesn't print more lines just changes the original print
    removed count if statement
    removed results parse in `find_results`
    probably more things

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 #main.py
+from datetime import datetime
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 
 from bs4 import BeautifulSoup
 
-DRIVER_PATH = "'your driver path"
+DRIVER_PATH = "C:\\Users\\rmrey\\shitty apps\\ChromeDriver\\chromedriver.exe"
 main_url = 'https://www.escapefromtarkov.com/cash'
 
 options = Options()
@@ -13,20 +14,21 @@ options.add_argument("--window-size=1920,1200")
 
 driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
 
-def find_results(url, result):
+def find_results(url):
     driver.get(url)
     soup = BeautifulSoup(driver.page_source, 'html.parser')
-
     titles = soup.find_all(class_='title')
-    count = 0
-    for title in titles:
-        result.extend(title.find(class_='text'))
-        if count < 1:
-            result.extend(soup.find(class_='count'))
-            result.extend(soup.find(class_='count_left'))
-            count += 1
+
+    result = [title.find(class_='text').text for title in titles]
+    result.extend(soup.find(class_='count'))
+    result.extend(soup.find(class_='count_left'))
 
     return result
 
 if __name__ == '__main__':
-    print(find_results(main_url, []))
+    while True:
+        driver.refresh()
+        main_result =datetime.now(), '--',find_results(main_url)
+        final_result = ' '.join(str(v) for v in main_result)
+
+        print(final_result, end='\r')
